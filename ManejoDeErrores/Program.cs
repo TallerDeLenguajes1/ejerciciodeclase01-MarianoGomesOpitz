5db5b707810cdd809d6e3a76f3927f0a840d79a9
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-08-26 17:28:10 -0300

    1era Corrección

--- ManejoDeErrores/Program.cs
+++ ManejoDeErrores/Program.cs
@@ -13,11 +13,23 @@ namespace ManejoDeErrores
             list[3] = "Wednesday";
             list[4] = "Thursday";
 
-            for (int i = 0; i <= 5; i++)
+            try
             {
-                Console.WriteLine(list[i].ToString());
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(list[i].ToString());
+                }
             }
-            Console.ReadLine();
+            catch (IndexOutOfRangeException x)
+            {
+                Console.WriteLine($"\nError detectado: \n{x}");
+            }
+            finally
+            {
+                Console.WriteLine("\nPresione ENTER para finalizar");
+                Console.ReadLine();
+            }
+
         }
     }
 }
