a03428e7780b374f7972b1da17b9d23a32bb8235
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-08-26 17:34:48 -0300

    2da Corrección

--- Problema02/Program.cs
+++ Problema02/Program.cs
@@ -11,9 +11,21 @@ namespace Problema02
 
             num1 = 30;
             num2 = 60;
-            result = Convert.ToByte(num1 * num2);
-            Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
-            Console.ReadLine();
+            
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            }
+            catch (OverflowException x)
+            {
+                Console.WriteLine($"\nError detectado: \n{x}");
+            }
+            finally
+            {
+                Console.WriteLine("\nPresione ENTER para finalizar");
+                Console.ReadLine();
+            }
         }
     }
 }
